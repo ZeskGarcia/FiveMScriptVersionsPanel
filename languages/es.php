60978af20c9e5832ec90fc2bcd535fc9f0af5312
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-11-02 14:04:32 +0100

    Create es.php

---
+++ languages/es.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang = array();
+
+$lang['usernameInput'] = "Usuario";
+$lang['passwordInput'] = "Contrase&ntilde;a";
+$lang['loginButton'] = "Iniciar Sesi&oacute;n";
+$lang['loginPageTitle'] = "Inicio de Sesi&oacute;n";
