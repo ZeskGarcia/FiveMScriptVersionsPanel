9586de354c1d1d86821650752892dcb2ba8edcad
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-11-02 14:17:08 +0100

    Update config.php

--- config.php
+++ config.php
@@ -11,7 +11,13 @@ $config['availableLangs'] = ['en', 'es'];
 $config['defaultLang'] = 'en';
 $config['userLang'] = in_array(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2), $config['availableLangs']) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) : $config['defaultLang'];
 
-
 // Users config
 
 $config['allowRegistrations'] = true;
+
+// Paths
+
+$config['paths'] = array(
+  "login" => "login",
+  "register" => "register"
+);
