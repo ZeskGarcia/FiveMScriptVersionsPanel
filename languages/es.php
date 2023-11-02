b07fd7d195fc7a3872bb66c8c8cc9c3803770012
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-11-02 14:14:44 +0100

    Update es.php

--- languages/es.php
+++ languages/es.php
@@ -6,3 +6,5 @@ $lang['usernameInput'] = "Usuario";
 $lang['passwordInput'] = "Contrase&ntilde;a";
 $lang['loginButton'] = "Iniciar Sesi&oacute;n";
 $lang['loginPageTitle'] = "Inicio de Sesi&oacute;n";
+$lang['notHavingAccount'] = "Todav&iacute;a no tienes una cuenta?";
+$lang['registerNow'] = "Reg&iacute;strate ahora!";
