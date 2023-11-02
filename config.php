9783b5332f415fb43e0c81e0895245732473da66
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-11-02 14:12:32 +0100

    Update config.php

--- config.php
+++ config.php
@@ -10,3 +10,8 @@ $config['siteName'] = "FiveM Script Versions";
 $config['availableLangs'] = ['en', 'es'];
 $config['defaultLang'] = 'en';
 $config['userLang'] = in_array(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2), $config['availableLangs']) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) : $config['defaultLang'];
+
+
+// Users config
+
+$config['allowRegistrations'] = true;
